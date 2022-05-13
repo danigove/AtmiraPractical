dac64c38335d7b453a6be1219a0db04facad4dbb
danigove <dani5002@hotmail.com>
2022-05-13 16:38:52 +0200

    Peticion base a api nasa

--- src/main/java/es/springboot/dangomvel/NasaRestEndpoint.java
+++ src/main/java/es/springboot/dangomvel/NasaRestEndpoint.java
@@ -1,19 +1,42 @@
 package es.springboot.dangomvel;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class NasaRestEndpoint {
 	
-	@GetMapping("/asteroids")
-	public String getAsteroidsNasa(@RequestParam @Min(1) @Max(7)int days){
+	@Value("${API_KEY}") 
+	private String apiKey;
+	
+	@GetMapping("/asteroids") 
+	public String getAsteroidsNasa(@RequestParam(required = true) int days){
 		
-		return "Dias: " + days;
+		return consumeNasaApi(days);
 	}
 
+	
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public String handleMissingParams(MissingServletRequestParameterException ex) {
+	    String name = ex.getParameterName();
+
+	    return name + " parameter is missing";
+	}
+	
+	
+	private String consumeNasaApi(int days) {
+		
+		//TODO: Formatear dates con days
+		String url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-12-09&end_date=2021-12-12&api_key=" + apiKey;
+		
+		RestTemplate restTemplate = new RestTemplate();
+	    String result = restTemplate.getForObject(url, String.class);	
+	    return result;
+		
+	}
 }
